ebd9abcb50109d7ec0f87510132cfabeda2d42a6
manchalaharikesh <manchalaharikesh@gmail.com>
2022-07-19 00:12:00 +0530

    removed changes done with qr error correction

--- src/qrcode_generator.py
+++ src/qrcode_generator.py
@@ -3,7 +3,7 @@ import pyqrcode
 
 def generate_qrcode(contents, filename):
 
-    qrcode = pyqrcode.create(contents, error='L')
+    qrcode = pyqrcode.create(contents)
     qrcode.png('./static/qrcodes/'+filename+'.png')
 
     return True
