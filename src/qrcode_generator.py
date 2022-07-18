71b96f0a1712ae5660d5d67313196b9c1dcaf67d
manchalaharikesh <manchalaharikesh@gmail.com>
2022-07-19 00:40:24 +0530

    fixed qr error correction heroku bug

--- src/qrcode_generator.py
+++ src/qrcode_generator.py
@@ -1,9 +1,8 @@
-from tkinter import Scale
 import pyqrcode
 
 def generate_qrcode(contents, filename):
 
-    qrcode = pyqrcode.create(contents)
+    qrcode = pyqrcode.create(contents, error='L')
     qrcode.png('./static/qrcodes/'+filename+'.png')
 
     return True
